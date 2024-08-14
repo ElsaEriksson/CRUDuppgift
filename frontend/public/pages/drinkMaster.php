e8ad2f82dae2770a5caeaf8476870232a4250eb1
Elsa Eriksson <elsa.95.eriksson@gmail.com>
2024-08-14 19:29:20 +0200

    csv button

--- frontend/public/pages/drinkMaster.php
+++ frontend/public/pages/drinkMaster.php
@@ -1,9 +1,9 @@
 <?php
-require_once ("public/utils/UrlModifier.php");
-require_once ("public/pages/layout/head.php");
-require_once ("public/pages/layout/nav.php");
-require_once ("public/pages/layout/bootstrapScripts.php");
-require_once (__DIR__ . "/../services/getProducts.php");
+require_once("public/utils/UrlModifier.php");
+require_once("public/pages/layout/head.php");
+require_once("public/pages/layout/nav.php");
+require_once("public/pages/layout/bootstrapScripts.php");
+require_once(__DIR__ . "/../services/getProducts.php");
 
 $urlModifier = new UrlModifier();
 
@@ -24,12 +24,21 @@ layout_head("Elsas Bank");
         <?php
         functions_getProducts($products);
         ?>
+        <div class="container--csvButton">
+            <button class="productButton" id="csvButton" onclick="downloadCSV()">CSV</button>
+        </div>
     </main>
 
     <!-- Scripts-->
     <?php
     layout_bootstrapScripts();
     ?>
+
+    <script>
+        function downloadCSV() {
+            window.location.href = "http://localhost:3000/products/export/csv";
+        }
+    </script>
 </body>
 
 </html>
--- frontend/public/pages/products.php
+++ frontend/public/pages/products.php
@@ -1,11 +1,10 @@
 <?php
-require_once (__DIR__ . "/../utils/UrlModifier.php");
-require_once (__DIR__ . "/layout/head.php");
-require_once (__DIR__ . "/layout/nav.php");
-require_once (__DIR__ . "/layout/bootstrapScripts.php");
-require_once (__DIR__ . "/../services/getProducts.php");
-require_once (__DIR__ . "/../functions/showProducts.php");
-
+require_once(__DIR__ . "/../utils/UrlModifier.php");
+require_once(__DIR__ . "/layout/head.php");
+require_once(__DIR__ . "/layout/nav.php");
+require_once(__DIR__ . "/layout/bootstrapScripts.php");
+require_once(__DIR__ . "/../services/getProducts.php");
+require_once(__DIR__ . "/../functions/showProducts.php");
 
 $urlModifier = new UrlModifier();
 
@@ -26,12 +25,22 @@ layout_head("Elsas Bank");
         <?php
         functions_showProducts($products);
         ?>
+        <div class="container--csvButton">
+            <button class="productButton" id="csvButton" onclick="downloadCSV()">CSV</button>
+        </div>
     </main>
 
     <!-- Scripts-->
     <?php
     layout_bootstrapScripts();
     ?>
+
+    <script>
+        function downloadCSV() {
+            window.location.href = "http://localhost:3000/products/export/csv";
+        }
+    </script>
+
 </body>
 
 </html>
